a98be34455703b728c379ea4382a13a06ac85ef8
GitHub-NoctuG <49110788+NoctuG@users.noreply.github.com>
2024-01-22 23:09:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Print user and root passwords
+echo "User password: $USER_PASSWORD"
+echo "Root password: $ROOT_PASSWORD"
+
 # Ensure the VNC password directory exists
 mkdir -p $HOME/.vnc
 
@@ -7,4 +11,4 @@ mkdir -p $HOME/.vnc
 vncserver :0 -geometry 1360x768 -depth 24 -localhost no &
 
 # Start noVNC
-/noVNC/utils/novnc_proxy  --vnc localhost:2000 --listen 8900
+/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 8900
