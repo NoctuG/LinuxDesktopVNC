15362d16439a351384d9a526e3d9f60444ab34ad
GitHub-NoctuG <49110788+NoctuG@users.noreply.github.com>
2024-01-22 22:28:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 # Ensure the VNC password directory exists
 mkdir -p $HOME/.vnc
 
-# Set the VNC password
-echo $USER_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
-
 # Start VNC server
 vncserver :0 -geometry 1360x768 -depth 24 -localhost no &
 
